23a5eaea4981752b5ee5c4f842a0d58e01b5dd56
AmineMabrouk17 <4m1nemabr0uk@gmail.com>
2026-07-28 20:24:52 +0100

    style: add sidebar scroll for long watchlists

--- apps/web/components/dashboard/DashboardGrid.tsx
+++ apps/web/components/dashboard/DashboardGrid.tsx
@@ -23,9 +23,13 @@ export function DashboardGrid() {
         collapsed ? "lg:grid-cols-[260px_1fr_56px]" : "lg:grid-cols-[260px_1fr_320px]"
       }`}
     >
-      <aside className="flex flex-col gap-3">
-        <AssetSearchBar />
-        <Watchlist />
+      <aside className="flex flex-col">
+        <div className="flex-shrink-0 pb-3">
+          <AssetSearchBar />
+        </div>
+        <div className="flex-1 overflow-y-auto">
+          <Watchlist />
+        </div>
       </aside>
 
       <main className="flex flex-col gap-4">
